25c2b8c763aa35afc93fb4909ca1fcd37fce8ebb
caoda <caoda@enn.cn>
2022-05-01 17:38:17 +0800

    对cloud-server实现灰度（多版本）

--- cloud-client/src/main/java/org/cyd/cloud/client/CloudClientApplication.java
+++ cloud-client/src/main/java/org/cyd/cloud/client/CloudClientApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -46,17 +49,14 @@ public class CloudClientApplication {
         return forObject;
     }
 
-    @RequestMapping("/serviceUrl")
-    public String serviceUrl() {
+    @RequestMapping("/serviceUrl/{version}")
+    public String serviceUrl(@PathVariable("version")String version) {
         List<ServiceInstance> list = discoveryClient2.getInstances("cloud-server");
         if (list != null && list.size() > 0 ) {
-            System.out.println("serviceId:"+list.get(0).getServiceId());
-            System.out.println("InstanceId:"+list.get(0).getInstanceId());
-            System.out.println("Uri:"+list.get(0).getUri().toString());
-            System.out.println("Host:"+list.get(0).getHost());
-            System.out.println("Uri:"+list.get(0).getScheme());
-            System.out.println("Metadata:"+list.get(0).getMetadata());
-            return list.get(0).getServiceId();
+
+            Optional<ServiceInstance> first = list.stream().filter(a -> a.getMetadata().containsKey("version") && a.getMetadata().get("version").equals(version)).findFirst();
+            ServiceInstance serviceInstance = first.orElse(new EurekaServiceInstance(InstanceInfo.Builder.newBuilder().getRawInstance()));
+            return serviceInstance.getInstanceId();
 
         }
         return null;
